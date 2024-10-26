328fa26c069d7d1276add5c40bd9ec7015d58737
HugoW5 <hugo05.wakt@gmail.com>
2024-10-26 21:21:56 +0200

    Add project files.

---
+++ Labb2_Arv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.Emit;
+
+namespace Labb2_Arv
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+
+	abstract class Animal
+	{
+		public string Classification { get; set; }
+		public string Breed { get; set; }
+		public string MovementType { get; }
+		public double Speed { get; set; }
+		public string Diet { get; set; }
+		public virtual void MakeSound()
+		{
+			Console.WriteLine("No Sound");
+		}
+		public virtual void Move()
+		{
+			Console.WriteLine($"{Breed} {MovementType}s at {Speed}km/h");
+		}
+		public virtual void Eat()
+		{
+			Console.WriteLine($"{Breed} eats {Diet}");
+		}
+	}
+
+	abstract class Dog : Animal
+	{
+		public string Name { get; set; }
+		public Dog() : base ()
+		{
+
+		}
+	}
+
+
+
+}
