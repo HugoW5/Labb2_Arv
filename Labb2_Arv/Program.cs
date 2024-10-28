f75d9b384e2cd605ddab0d5ea501f30cce11e554
HugoW5 <hugo05.wakt@gmail.com>
2024-10-28 12:11:24 +0100

    Added Fish:Animal, and Salmon:Animal

--- Labb2_Arv/Program.cs
+++ Labb2_Arv/Program.cs
@@ -6,39 +6,107 @@ namespace Labb2_Arv
 	{
 		static void Main(string[] args)
 		{
+			Salmon salmon = new Salmon(10);
+			Dog dog = new Dog("Bulldog");
+			Console.WriteLine(salmon.ToString());
+			Console.WriteLine(dog.ToString());
+
 		}
 	}
 
 	abstract class Animal
 	{
-		public string Classification { get; set; }
-		public string Breed { get; set; }
-		public string MovementType { get; }
-		public double Speed { get; set; }
-		public string Diet { get; set; }
+		public string Species { get; set; } = "N/A";
+		public string MovementType { get; set; } = "N/A";
+		public double Speed { get; set; } = 0;
+		public string Diet { get; set; } = "N/A";
+		public string Sound { get; set; } = "N/A";
 		public virtual void MakeSound()
 		{
-			Console.WriteLine("No Sound");
+			Console.WriteLine(Sound);
 		}
 		public virtual void Move()
 		{
-			Console.WriteLine($"{Breed} {MovementType}s at {Speed}km/h");
+			Console.WriteLine($"{MovementType}s at {Speed}km/h");
 		}
 		public virtual void Eat()
 		{
-			Console.WriteLine($"{Breed} eats {Diet}");
+			Console.WriteLine($"eats {Diet}");
 		}
-	}
-
-	abstract class Dog : Animal
-	{
-		public string Name { get; set; }
-		public Dog() : base ()
+		public override string ToString()
 		{
+			string s = $"--{this.GetType().Name}--\n";
 
+			foreach (var property in this.GetType().GetProperties())
+			{
+				s += $"{property.Name}: {property.GetValue(this)}\n";
+			}
+			return s;
 		}
 	}
 
+	class Dog : Animal
+	{
+		public string Breed { get; set; } = "N/A";
+		public Dog(string initialBreed)
+		{
+			base.Species = "Dog";
+			base.MovementType = "Run";
+			base.Diet = "Dog Food";
+			base.Speed = 20;
+			base.Sound = "Woof";
+			Breed = initialBreed;
+		}
+		public override void Move()
+		{
+			Console.Write(Breed + " ");
+			base.Move();
+		}
+		public override void Eat()
+		{
+			Console.Write(Breed + " ");
+			base.Eat();
+		}
+		public void Sit()
+		{
+			Console.WriteLine("Dog is sitting");
+		}
+	}
 
+	abstract class Fish : Animal
+	{
+		public string WaterType { get; set; } = "N/A";
+		public bool IsPredator { get; set; } = true;
+		public Fish()
+		{
+			base.Species = "N/A";
+			base.MovementType = "Swin";
+			base.Diet = "N/A";
+			base.Speed = 0;
+			base.Sound = "bolb-blob";
+		}
+	}
+
+	class Salmon : Fish
+	{
+		public Salmon(int speed)
+		{
+			Species = "Salmon";
+			base.Speed = speed;
+			WaterType = "Freshwater";
+			IsPredator = true;
+			Diet = "Small Fish";
+		}
+		public override void Move()
+		{
+            Console.Write(Species);
+			base.Move();
+		}
+		public override void Eat()
+		{
+			Console.Write(Species + " ");
+			base.Eat();
+		}
+	}
 
 }
